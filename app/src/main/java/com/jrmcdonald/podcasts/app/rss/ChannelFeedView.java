85085b535dedef01cb12fd537a55252fbc587613
Jamie McDonald <jamie@qwyck.net>
2019-02-11 15:46:23 +0000

    Remove unused imports.

--- app/src/main/java/com/jrmcdonald/podcasts/app/rss/ChannelFeedView.java
+++ app/src/main/java/com/jrmcdonald/podcasts/app/rss/ChannelFeedView.java
@@ -1,9 +1,7 @@
 package com.jrmcdonald.podcasts.app.rss;
 
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
